65a4c571f519c500dfdecc9984e559671c271d2d
Diya Bhandari <diyabhandari@gmail.com>
2024-10-27 21:06:02 +0530

    fix welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //equivalent to app.js of tutorial, import the dashboard here
 
 import React, {useState, useEffect, useLayoutEffect} from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import {supabase} from "./createClient"
 import Header from './components/header.jsx'
 import AuctionCard from './components/auctionCard.jsx'
@@ -13,6 +13,17 @@ import { UserProvider } from "./UserContext.jsx";
 import UserDashboard from "./pages/userDashboard.jsx";
 import WelcomePage from "./pages/welcomePage.jsx";
 const App = () => {
+  const [hasVisited, setHasVisited] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the user has visited the app already
+    if (!hasVisited) {
+      // If not, navigate to the welcome page and set the flag
+      navigate('/welcomePage');
+      setHasVisited(true); // Ensure it only happens once
+    }
+  }, [hasVisited, navigate]);
   /*const [items,setItems]=useState([])
   console.log(items) //what are the values of items
   useEffect(()=> { //we want the fetch function to be called every time we run the project
@@ -32,8 +43,8 @@ const App = () => {
     <>
     <UserProvider> 
       <Header />
-      <WelcomePage />
       <Routes>
+        <Route path="/welcomePage" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/userDashboard" element={<UserDashboard />} />
